a65f369de761f00681b1be643715736fc4987162
lee <dldbsgh00005@gmail.com>
2026-05-22 14:16:42 +0900

    fix: resolve multiple main classes error for Docker build

--- src/main/java/com/project/ds_helper/DsHelperAdminApplication.java
+++ src/main/java/com/project/ds_helper/DsHelperAdminApplication.java
@@ -11,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class DsHelperAdminApplication {
 
 	public static void main(String[] args) {
+		java.util.TimeZone.setDefault(java.util.TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(DsHelperAdminApplication.class, args);
 	}
 
